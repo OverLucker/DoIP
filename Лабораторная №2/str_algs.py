ba9659b18cb7bed8c43f522566c05bb2670aa1d6
OverLucker <lucker1005000@gmail.com>
2016-09-16 16:19:42 +0300

    Второе задание второй лабы

---
+++ Лабораторная №2/str_algs.py
@@ -0,0 +1,10 @@
+﻿def palindrom(str):
+    new_str = ""
+    for i in str:
+        new_str = i + new_str
+    return new_str
+    
+if __name__ == "__main__":
+    str = "hello, world!"
+    print("Исходная строка: " + str)
+    print(palindrom(str))
