450342eff9f6844c728faa1a0c708a9428f3e0c6
OverLucker <lucker1005000@gmail.com>
2016-09-16 16:03:47 +0300

    Первое задание второй лабы

---
+++ Лабораторная №2/arr_algs.py
@@ -0,0 +1,19 @@
+﻿def min(arr):
+    min = arr[0]
+    for i in arr:
+        if min > i:
+            min = i
+    return min
+    
+def avg(arr):
+    avg = 0
+    for i in arr:
+        avg = avg + i
+    avg = avg / len(arr)
+    return avg
+    
+if __name__ == "__main__":
+    arr = [1, 2, 4, 5]
+    print(arr)
+    print('Минимум ' + str(min(arr)))
+    print('Среднее ' + str(avg(arr)))
