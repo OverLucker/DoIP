f52c64911e8b41940d4a60e857484dd742d5f0af
OverLucker <lucker1005000@gmail.com>
2016-12-20 21:32:51 +0300

    lab5 work finished

--- lab5/main/views.py
+++ lab5/main/views.py
@@ -1,29 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Event
 
-# Create your views here.
-def simple_request (request) :
-    return render ( request, 'main/index.html' )
-    
-def orders_request (request) :
-    #return HttpResponse("hello world!")
-    context = {
-        'orders' : [
-            {
-                'id' : 1,
-                'title' : 'Первый заказ',
-            },
-            {
-                'id' : 2,
-                'title' : 'Второй заказ',
-            },
-            {
-                'id' : 3,
-                'title' : 'Третий заказ',
-            },
-        ]
-    }
-    return render ( request, 'main/orders.html', context )
-    
-def order_request( request, id ) :
-    return HttpResponse( "hello world!" )
+
+def object_list(request, page_id):
+    objects_on_list = 10
+    page_id = int(page_id if page_id else 0)
+    end = len(Event.objects.all())
+    tmp = end - objects_on_list * page_id
+    end = tmp if tmp > 0 else 0
+    tmp = end - objects_on_list
+    start = tmp if tmp > 0 else 0
+    lst = Event.objects.all()[start:end:-1]
+    return render(
+        request,
+        'index.html',
+        context={
+            'page': {'title': 'List'},
+            'list': lst,
+        }
+    )
+
+
+def object(request, object_id):
+    return render(
+        request,
+        'single_object.html',
+        context={
+            'object': get_object_or_404(Event, id=object_id),
+        }
+    )
